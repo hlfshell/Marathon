2eb4a300864d86b68af2103dfcd6367e3d49f049
Keith <kchester@thisisfusion.com>
2014-02-06 16:53:04 -0600

    initial commit

---
+++ application.js
@@ -0,0 +1,193 @@
+var async = require('async')
+
+module.exports = function(docker){
+
+	function Application(name, image){
+		this.name = name
+		this.containers = []
+		this.startedAt = null
+	}
+
+	/*
+	* containerStatuses( callback(err, statuses) )
+	*
+	* Check the status of the application.
+	* Expected return:
+	* [ { container: containerObj, status: true/false } ]
+	*/
+	Application.prototype.containerStatuses = function(cb){
+		var self = this
+		if(fs.existsSync(__dirname + '/apps/' + self.name + '/status.lock')){
+			//Because the status is locked, we return a in-progress notification
+			cb('Status is being checked already', null)
+			return
+		} else {
+			fs.writeFileSync(__dirname + '/apps/' + self.name + '/status.lock', 'Lock ALL the statuses! Eh - actually, wait - just this one')
+		}
+
+		var containerStatuses = []
+		async.each(self.containers, function(container, done){
+
+			container.inspect(function(err, inspect){
+				if(err){
+					done(err)
+				} else {
+					containerStatuses.push({
+						container: container,
+						status: inspect.State.Running,
+						startedAt: inspect.State.StartedAt
+					})
+					done(null)
+				}
+			})
+
+		}, function(err){
+			//Release lock
+			fs.unlinkSync(__dirname + '/apps/' + self.name + '/status.lock')
+			//Return!
+			cb(err, containerStatuses)
+		})
+	}
+
+	/*
+	* launchContainer( callback(err, container) )
+	*
+	* Adds a container to the object, including updating the container
+	* file in our app folder.
+	* Note - this uses a lock for transactions issues, so it might be
+	* delayed
+	*
+	* Returns error, new container
+	*/
+	Application.prototype.launchContainer(cb){
+		var self = this
+		if(fs.existsSync(__dirname + '/apps/' + self.name + '/containers.lock')){
+			//Because the containers isdo file locked, we set a timeout, return,
+			//and the timeout retries this until unlocked.
+			setTimeout(self.launchContainer(cb), 10)
+			return
+		} else {
+			fs.writeFileSync(__dirname + '/apps/' + self.name + '/containers.lock', 'Such container. Much Lock. Wow.')
+		}
+
+		async.waterfall([
+
+			//create the container
+			function(done){
+				docker.createContainer({
+					image: self.image
+				}, function(err, container){
+					done(err, container)
+				})
+			},
+
+			//Read the container file for this app
+			function(container, done){
+				fs.readFile(__dirname + '/apps/' + self.name + '/containers', function(err, data){
+					if(err){
+						done(err)
+					} else {
+						done(null, container, JSON.parse(data))
+					}
+				})
+			},
+
+			//Append the container to the containers and save!
+			function(container, containers, done){
+				containers.push(container)
+				self.containers = containers
+				fs.writeFile(__dirname + '/apps/' + self.name + '/containers', JSON.stringify(containers), function(err){
+					done(err, container)
+				}
+			}
+
+		], function(err, container){
+			//Release lock
+			fs.unlinkSync(__dirname + '/apps/' + self.name + '/containers.lock')
+			//Return!
+			cb(err, container)
+		})
+	}
+
+	/*
+	* loadContainers( callback(err, containers) )
+	*
+	* Loads containers from json file containers,
+	* returns containers. Note - affected by container
+	* lock
+	*/
+	Application.prototype.loadContainers(cb){
+		var self = this
+		if(fs.existsSync(__dirname + '/apps/' + self.name + '/containers.lock')){
+			//Because the containers isdo file locked, we set a timeout, return,
+			//and the timeout retries this until unlocked.
+			setTimeout(this.launchContainer(cb), 10)
+			return
+		} else {
+			fs.writeFileSync(__dirname + '/apps/' + self.name + '/containers.lock', 'Such container. Much Lock. Wow.')
+		}
+
+		async.waterfall([
+
+			//Read the container file for this app
+			function(done){
+				fs.readFile(__dirname + '/apps/' + self.name + '/containers', function(err, data){
+					if(err){
+						done(err)
+					} else {
+						done(null, JSON.parse(data))
+					}
+				})
+			},
+
+			//For each containers ID, try and find the matching container
+			//in docker. Load as dockerode container object
+			function(savedContainers, done){
+				containers = []
+				async.each(savedContainers, function(savedContainer, done){
+
+					var container = docker.getContainer(savedContainer.id)
+					if(container){
+						containers.push(container)
+					}
+					done(null)
+
+				}, function(err){
+					if(!err){
+						self.containers = containers
+					}
+					done(err, containers)
+				})
+			}
+
+
+		] function(err, containers){
+			//Release lock
+			fs.unlinkSync(__dirname + '/apps/' + self.name + '/containers.lock')
+			//Return!
+			cb(err, containers)
+		})
+	}
+
+	//TODO: comment
+	Application.prototype.daemonFocussed(){
+		if(fs.existsSync(__dirname + '/apps/' + self.name + '/daemon.lock')){
+			return false
+		} else {
+			fs.writeFileSync(__dirname + '/apps/' + self.name + '/daemon.lock', 'You have the eye of the daemon upon you')
+			return true
+		}
+	}
+
+	//TODO: comment
+	Application.prototype.daemonUnfocussed(){
+		if(!fs.existsSync(__dirname + '/apps/' + self.name + '/daemon.lock')){
+			return false
+		} else {
+			fs.unlinkSync(__dirname + '/apps/' + self.name + '/daemon.lock')
+			return true
+		}
+	}
+
+	return Application
+}
---
+++ daemon.js
@@ -0,0 +1,151 @@
+var async = require('async')
+
+var swig = require('swig')
+
+var Docker = require('dockerode')
+var docker = new Docker({socketPath: '/var/run/docker.sock'})
+
+//models
+var Application = require('./application')(docker)
+
+var portscanner = require('portscanner')
+
+var fs = require('fs-extra')
+//Ensure that we have the correct folders created. Synchronous on purpose
+if(!fs.exists(__dirname + '/apps')){
+	//Allow error on this to bubble up and crash - only real error
+	//is we don't have write access
+	fs.mkdirSync(__dirname + '/apps')
+}
+
+//Build up our repertoire of apps
+var appsFiles = fs.readdirSync(__dirname + '/apps')
+var appsFolders = []
+appFiles.forEach(function(appFile){
+	if(appsFile.statSync().isDirectory()){
+		appsFolders.push(appsFile)
+	}
+})
+delete appsFiles
+
+//We have app folders. Build the app objects.
+//App object contains name (dir name) and ids for containers
+var applications = []
+appsFolders.forEach(function(appFolder){
+
+	//clear out all known tmp files that were used for locking status
+	var locks = [ 'containers', 'settings', 'status', 'daemonsEye']
+	locks.forEach(function(lock){
+		if(fs.existsSync(__dirname + '/apps/' + appFolder + '/' + lock + '.lock')){
+			fs.unlinkSync(__dirname + '/apps/' + appFolder + '/' + lock + '.lock')
+		}
+	})
+
+	//Inside of each app folder is two files and a folder.
+	//Folder is a repos of code as is
+	//Files are: 1) container information and 2) app configuration info
+	var app = {
+		name: appFolder,
+		conf: JSON.parse(fs.readFileSync(__dirname + '/apps/' + appFolder + '/conf')),
+		containers: JSON.parse(fs.readFileSync(__dirname + '/apps/' + appFolder + '/containers')))
+	}
+
+	applications.push(app)
+})
+delete appFolders
+
+//Don't try to launch apps if nginx is down. Also - engaged sounds cool.
+var nginxEngaged = false
+var nginx = require('nginx-vhosts')(
+	{
+		confDir: '/etc/nginx/sites-enabled',
+		pidLocation: '/var/run/nginx.pid'
+	},
+	function(nginxStatus){
+		if(nginxStatus){
+			nginxEngaged = true
+		} else {
+			//If the status is false, kill? Don't know. Might be good to fail gracefully or fire alerts
+			nginxEngaged = false
+		}
+	}
+)
+
+//TODO - read command line arguments
+
+var daemon = function(){
+
+	var checkOnApp = function(){
+		//If nginx is off, we must not do anything
+		if(!nginxEngaged){
+			setTimeout(checkOnApp, 100)
+			return
+		}
+
+		async.each(applications, function(application, done){
+
+			async.waterfall([
+
+				function(done){
+					if(application.daemonFocussed()){
+						done(null)
+					} else {
+						done('Daemon is already focussed on this')
+					}
+				}
+
+				function(done){
+					application.containerStatuses(function(err, statuses){
+						done(err, statuses)
+					})
+				},
+
+				function(statuses, done){
+					var downContainers = []
+					async.each(statuses, function(status, done){
+
+						if(!status.status){
+							downContainers.push(status.container)
+						}
+
+					}, function(err){
+						done(err, downContainers)
+					})
+				},
+
+				//For each downContainer, bring it back up
+				function(downContainers, done){
+					async.each(downContainers, function(downContainer, done){
+
+						//TODO - load confs somehow?
+						downContainer.start(TODO, function(err, data){
+							//Do anything with err or data?
+							done(null)
+						})
+
+					}, function(err){
+						done(err)
+					})
+				},
+
+				//Check to see if we have two containers loaded for this application. If not, then we need to create one!
+				//TODO
+
+			], function(err){
+				application.daemonUnfocussed()
+				done(null)
+			})
+
+		}, function(err){
+			//TODO - better error handling/logging/reporting
+			if(err){
+				console.log("Error!", err)
+			} else {
+				setTimeout(checkOnApp, 100)
+			}
+		})
+	}
+
+	//Start checking
+	checkOnApp()
+}
